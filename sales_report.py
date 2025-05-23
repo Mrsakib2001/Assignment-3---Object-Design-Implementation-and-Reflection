306a1e3ae393e6e40e56c5b869d38967f863aa67
Archit <archit.subhu@gmail.com>
2025-05-23 16:38:45 +1000

    Starting Fresh
    got rid of all the files that were there

--- sales_report.py
+++
@@ -1,6 +0,0 @@
-class SalesReport:
-    def generate(self):
-        print("\nSales Report:")
-        print("Laptop - 3 sold")
-        print("Mouse - 5 sold")
-        print("Total Revenue: $5000")
--- user_account.py
+++
@@ -1,9 +0,0 @@
-class UserAccount:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-    def save(self):
-        with open("accounts.txt", "a") as file:
-            file.write(f"{self.email},{self.password}\n")
-        print("Account created and saved.")
